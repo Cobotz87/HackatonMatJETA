818b69945aa5dd7862c56b8e6cc555c58077dca6
corpora15 <hoongjoshua@gmail.com>
2015-05-29 19:32:12 +0800

    Changes on Name Entity Detection

--- app/src/main/java/com/jeta/materialise/languageprocessor/MainProcessor.java
+++ app/src/main/java/com/jeta/materialise/languageprocessor/MainProcessor.java
@@ -10,15 +10,26 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
+import java.util.ArrayList;
 
+import opennlp.tools.cmdline.PerformanceMonitor;
+import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.postag.POSTagger;
+import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.tokenize.WhitespaceTokenizer;
 import opennlp.tools.util.InvalidFormatException;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.Span;
 
 /**
@@ -53,5 +64,28 @@ public class MainProcessor {
         String[] outputNames = Span.spansToStrings(nameSpans, i_token);
         return outputNames;
     }
+
+    public static String tagPartOfSpeech(String input, InputStream file) throws IOException {
+        POSModel model = new POSModel(file);
+        POSTaggerME tagger = new POSTaggerME(model);
+
+        ObjectStream<String> lineStream = new PlainTextByLineStream(
+                new StringReader(input));
+
+        ArrayList<String> output_result = new ArrayList<String>();
+        String line;
+        String result;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = lineStream.read()) != null) {
+            String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE
+                    .tokenize(line);
+            String[] tags = tagger.tag(whitespaceTokenizerLine);
+            POSSample sample = new POSSample(whitespaceTokenizerLine, tags);
+            stringBuilder.append(sample.toString());
+        };
+        result = stringBuilder.toString();
+        return result;
+    }
+
 }
 
