bbcb6317dc1720fe692ff5fe239b8323c66e553d
corpora15 <hoongjoshua@gmail.com>
2015-05-30 09:47:51 +0800

    QueryData

--- app/src/main/java/com/jeta/materialise/presenter/MainActivityPresenter.java
+++ app/src/main/java/com/jeta/materialise/presenter/MainActivityPresenter.java
@@ -1,5 +1,6 @@
 package com.jeta.materialise.presenter;
 
+import android.content.Context;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -7,8 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.jeta.materialise.JETAapp;
 import com.jeta.materialise.adapter.ConversationAdapter;
+import com.jeta.materialise.languageprocessor.ContextDigester;
+import com.jeta.materialise.languageprocessor.ContextIntrepreter;
+import com.jeta.materialise.languageprocessor.QueryData;
 import com.jeta.materialise.matjeta.MainActivity;
+import com.jeta.materialise.model.Message;
 
 /**
  * Created by ArifHBtz on 5/29/2015.
@@ -19,6 +25,7 @@ public class MainActivityPresenter {
     private ConversationAdapter mConversationAdapter;
 
     private String mQuestion;
+    private ContextDigester mDigester;
 
     public MainActivityPresenter(MainActivity activity){
         mActivity = activity;
@@ -51,6 +58,37 @@ public class MainActivityPresenter {
             @Override
             public void onClick(View v) {
 
+                if(mQuestion.isEmpty())
+                    return;
+
+                Message userMsg = new Message("You",mQuestion);
+                JETAapp.getMessageManager().addMessage(userMsg);
+
+                JETAapp.getMainActivity().setBusy(true);
+                JETAapp.getMainActivity().handleBusy();
+
+                Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        String[] tokens = mDigester.getTokens(mQuestion);
+//                        String[] namesFound = mDigester.getNames(tokens);
+
+                        QueryData query = new QueryData();
+                        String answer = query.queryAnswer(mQuestion);
+                        final Message curr_message = new Message("Mia", answer);
+
+                        mActivity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                JETAapp.getMessageManager().addMessage(curr_message);
+                                JETAapp.getMainActivity().setBusy(false);
+                                JETAapp.getMainActivity().handleBusy();
+                                notifyConversationDataSetChanged();
+                            }
+                        });
+                    }
+                });
+                t.start();
             }
         });
     }
